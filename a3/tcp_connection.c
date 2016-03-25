113b91a5dcce6e610d545911ea32c8a13b2a6c59
g2minhle <lehoangminh@live.com>
2016-03-25 19:07:01 -0400

    Add tcp_connection

---
+++ a3/tcp_connection.c
@@ -0,0 +1,101 @@
+#include "tcp_connection.h"
+
+/*
+ * Establish a TCP connection given the host name and the TCP port.
+ *
+ * Args:
+ *    struct tcp_connection* tcp_con:
+ *      The tcp connection.
+ *    const char* host_name:
+ *      The server host name.
+ *    int port:
+ *      The server port.
+ */
+int init_tcp_connection(struct tcp_connection* tcp_con, 
+                        const char* host_name, 
+                        int port){
+  struct sockaddr_in addr;
+  struct hostent* host_entry;
+
+  /*get ip address of the host*/
+  host_entry = gethostbyname(host_name);
+
+  if (!host_entry){
+    return TCP_ERROR_CANNOT_RESOLVE_HOST;
+  } 
+
+  memset(&addr,0,sizeof(addr));
+  addr.sin_addr=*(struct in_addr *) host_entry->h_addr_list[0];
+  addr.sin_family=AF_INET;
+  addr.sin_port=htons(port);
+
+  /* open socket */
+  tcp_con->sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if(clientContext->sock < 0){
+    return TCP_CANNOT_BIND_TO_SOCKET;
+  }
+
+  if(connect(clientContext->sock,(struct sockaddr *)&addr, sizeof(addr)) < 0){
+    return TCP_CANNOT_CONNECT;
+  }
+
+  return NO_ERROR;
+}
+
+/*
+ * Send a tcp message throught the given tcp_connection and get the respond.
+ *
+ * Arg:
+ *    struct tcp_connection* tcp_con:
+ *      The tcp connection.
+ *    char* data:
+ *      The message content.
+ *    size_t data:
+ *      The size of the message.
+ *
+ * Return:
+ *    char*:
+ *      The pointer to the repond. 
+ *      If there is an error then we will return NULL.
+ */
+char* send_request(struct tcp_connection* tcp_con, 
+                    char* data, 
+                    size_t data,
+                    int* nerror) {
+  int io_result = write(sd, data, sizeof(data)) 
+  if( write(sd, data, sizeof(data)) < 0 ) {
+    *nerror = TCP_WRITE_ERROR;
+    return NULL;
+  }
+  
+  char* buf = (char*) malloc(TCP_BUFFER_SIZE);
+  size_t buf_used = 0;
+  
+  while(buf_used < TCP_BUFFER_SIZE) {
+    io_result  = read(tcp_con->sock, &buf[totalcnt], TCP_BUFFER_SIZE - buf_used);
+    if(io_result  < 0) {
+      *nerror = TCP_READ_ERROR;
+      free(buf);
+      return NULL;
+    }
+    else if (io_result  == 0) {
+      // No more data to read
+      break;
+    }
+    else {
+      buf_used +=  io_result ;
+    }
+  }
+  return buff;  
+}
+
+/*
+ * Close a TCP connection.
+ *
+ * Agrs:
+ *    struct tcp_connection* tcp_con:
+ *      The TCP connection.
+ */
+void close_tcp_connection(struct tcp_connection* tcp_con) {
+  close(tcp_con->sock);
+}
---
+++ a3/tcp_connection.h
@@ -0,0 +1,74 @@
+#ifndef TCP_CONNECTION_H
+#define TCP_CONNECTION_H
+
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <malloc.c>
+
+#define TCP_BUFFER_SIZE 640
+
+#define NO_ERROR 0
+#define TCP_ERROR_CANNOT_RESOLVE_HOST 1
+#define TCP_CANNOT_BIND_TO_SOCKET 2
+#define TCP_CANNOT_CONNECT 3
+#define TCP_WRITE_ERROR 4
+#define TCP_READ_ERROR 4
+
+/*
+ * Object represent a TCP connection
+ */
+struct tcp_connection {  
+	int sock;
+}
+
+
+/*
+ * Establish a TCP connection given the host name and the TCP port.
+ *
+ * Args:
+ *    struct tcp_connection* tcp_con:
+ *      The tcp connection.
+ *    const char* host_name:
+ *      The server host name.
+ *    int port:
+ *      The server port.
+ */
+int init_tcp_connection(struct tcp_connection* tcp_con, 
+                        const char* host_name, 
+                        int port);
+                        
+/*
+ * Send a tcp message throught the given tcp_connection and get the respond.
+ *
+ * Arg:
+ *    struct tcp_connection* tcp_con:
+ *      The tcp connection.
+ *    char* data:
+ *      The message content.
+ *    size_t data:
+ *      The size of the message.
+ *
+ * Return:
+ *    char*:
+ *      The pointer to the repond. 
+ *      If there is an error then we will return NULL.
+ */
+char* send_request(struct tcp_connection* tcp_con, 
+                    char* data, 
+                    size_t data,
+                    int* nerror);
+
+/*
+ * Close a TCP connection.
+ *
+ * Agrs:
+ *    struct tcp_connection* tcp_con:
+ *      The TCP connection.
+ */
+void close_tcp_connection(struct tcp_connection* tcp_con);
+
+#endif
