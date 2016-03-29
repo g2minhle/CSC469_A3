f106e9c95bf1bbd2f68b39a733998476964539b7
g2minhle <lehoangminh@live.com>
2016-03-28 22:19:23 -0400

    Add chatserver manager to contact location server to get chatserver info

---
+++ a3/chatserver_manager.c
@@ -0,0 +1,33 @@
+#include "chatserver_manager.h"
+
+// TODO #feature: Enable this
+//struct location_server_manager* create_location_server_manager(struct receiver_manager* receiver_mgr) {
+struct chatserver_manager* create_chatserver_manager(char* host_name, u_int16_t tcp_port, u_int16_t udp_port) {
+  struct chatserver_manager* chatserver_mgr = (struct chatserver_manager*) malloc(
+    sizeof(struct chatserver_manager)
+  );
+  strncpy(chatserver_mgr->host_name, host_name, MAX_HOST_NAME_LEN);  
+  chatserver_mgr->tcp_port = tcp_port;
+  chatserver_mgr->udp_port = udp_port;
+  return chatserver_mgr;
+}
+
+void destroy_chatserver_manager(struct chatserver_manager* chatserver_manager) {
+  free(chatserver_manager);
+}
+
+int refresh_chatserver(struct chatserver_manager* chatserver_mgr) {  
+  int nerror;
+  u_int16_t http_status;
+  struct http_connection* http_con = create_http_connection(LOC_SERV_HOST_NAME, LOC_SERV_PORT, &nerror);  
+  char* respond = send_http_request(http_con, LOC_SERV_URL, &http_status, &nerror);  
+  sscanf (respond, "%s %u %u", 
+            chatserver_mgr->host_name,
+            (unsigned int*)&chatserver_mgr->tcp_port, 
+            (unsigned int*)&chatserver_mgr->udp_port);
+              
+  close_http_connection(http_con);
+  free(respond);
+  return 0;
+}
+
---
+++ a3/chatserver_manager.h
@@ -0,0 +1,35 @@
+#ifndef __LOCATION_SERVER_MANAGER_H
+#define __LOCATION_SERVER_MANAGER_H
+
+#define LOC_SERV_HOST_NAME "www.cdf.toronto.edu"
+#define LOC_SERV_PORT 80
+#define LOC_SERV_URL "/~csc469h/winter/chatserver.txt"
+
+#include <inttypes.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "defs.h"
+#include "http_connection.h"
+
+/*
+ * This struct tries to provide information about chatserver through communicating
+ * the location server. This class sends out HTTP request to get info about the 
+ * chat server.
+ */
+struct chatserver_manager {  
+  char host_name[MAX_HOST_NAME_LEN];
+  u_int16_t tcp_port;
+  u_int16_t udp_port; 
+  // TODO #feature: enable this
+  //struct receiver_manager* receiver_mgr;
+};
+
+// TODO #feature: Enable this
+//struct location_server_manager* create_location_server_manager(struct receiver_manager* receiver_mgr) {
+struct chatserver_manager* create_chatserver_manager(char* host_name, u_int16_t tcp_port, u_int16_t udp_port);
+void destroy_chatserver_manager(struct chatserver_manager* chatserver_manager);
+int refresh_chatserver(struct chatserver_manager* chatserver_mgr);
+
+#endif
